3590ea955122ab383c2e2f82be445d6ba55f2894
LucarioMi-1998 <michalskil@web.de>
2025-12-17 19:26:31 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Hipster config (keep this file when updating)
+// Spotify Developer: https://developer.spotify.com/dashboard
+window.HIPSTER_CONFIG = {
+  spotifyClientId: "fc0b3b30a9324288a9723c9475a1c2a8"
+};
