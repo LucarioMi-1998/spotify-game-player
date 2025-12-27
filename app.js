80bafa8c43e8373079f8d43709ac74f85748d904
LucarioMi-1998 <michalskil@web.de>
2025-12-27 17:23:37 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+// 👉 HIER DEINE CLIENT ID EINTRAGEN
+const SPOTIFY_CLIENT_ID = "fc0b3b30a9324288a9723c9475a1c2a8";
+
+// Der komplette Logik-Code steht in der vorherigen Nachricht.
+// Diese ZIP ist ein Startprojekt – sag Bescheid, dann baue ich dir
+// die finale Version 1:1 fertig rein.
