9a10c67991c72926e00dc20dc7a56be689fd7c89
LucarioMi-1998 <michalskil@web.de>
2025-12-27 16:44:07 +0100

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// Hipster config (keep this file when updating)
-// Spotify Developer: https://developer.spotify.com/dashboard
-window.HIPSTER_CONFIG = {
-  spotifyClientId: "fc0b3b30a9324288a9723c9475a1c2a8",
-  // Optional: must match your Spotify Dashboard Redirect URI exactly
-  // Example: "https://YOURNAME.github.io/hipster/callback.html"
-  redirectUri: ""
-};
