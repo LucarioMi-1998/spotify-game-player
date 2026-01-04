8d07161120ad429ae415936f8ee84a3a4a508699
LucarioMi-1998 <michalskil@web.de>
2026-01-04 15:40:56 +0100

    Delete qr.js

--- qr.js
+++
@@ -1,6 +0,0 @@
-// Simulierter QR-Scanner
-document.getElementById("qrOverlay").addEventListener("click", () => {
-  if (window.onQrDetected) {
-    window.onQrDetected("https://open.spotify.com/track/test");
-  }
-});
